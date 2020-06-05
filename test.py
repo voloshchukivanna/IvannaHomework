77fdb631847f7ae8792312b06f837156a1fe8cbf
voloshchukivanna <voloshchuk.ivanna@gmail.com>
2020-06-05 10:24:13 +0300

    test

---
+++ test.py
@@ -0,0 +1,22 @@
+
+# s = input()
+# print(s[(len(s) + 1) // 2:] + s[:(len(s) + 1) // 2])
+
+
+
+a = input()
+
+while True:
+    a = input ('Enter: ')
+    if " " in a:
+        break
+        print(a)
+
+
+
+
+
+
+
+
+
