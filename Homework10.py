2afbd538d6506918d3dfbd9ca15d21de03fb579b
voloshchukivanna <voloshchuk.ivanna@gmail.com>
2020-08-28 21:45:43 +0300

    @elpiankova

--- Homework10.py
+++ Homework10.py
@@ -8,37 +8,37 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 
 
-#Task1 Выбор из выпадающего мекню
-# dr = webdriver.Chrome()
-# dr.get("https://www-archive.mozilla.org/projects/ui/accessibility/unix/testcase/html/")
-#
-# dropdown = dr.find_element(By.TAG_NAME, 'select')
-# dropdown.click()
-# time.sleep(2)
-#
-# dropdown.find_element(By.XPATH, '/option[3]')
-# dropdown.click()
-# time.sleep(2)
+'''Task1 Выбор из выпадающего мекню'''
+dr = webdriver.Chrome()
+dr.get("https://www-archive.mozilla.org/projects/ui/accessibility/unix/testcase/html/")
 
-#Task2 Выбор из выпадающего меню (Select)
-# dr = webdriver.Chrome()
-# dr.get("https://www-archive.mozilla.org/projects/ui/accessibility/unix/testcase/html/")
-#
-# select = Select(dr.find_element(By.ID, 'components'))
-# time.sleep(2)
-# select.select_by_value('Build config')
-# time.sleep(2)
+dropdown = dr.find_element(By.TAG_NAME, 'select')
+dropdown.click()
+time.sleep(2)
 
-#Task3 Checkbox ??????
-# dr = webdriver.Chrome()
-# dr.get("https://www-archive.mozilla.org/projects/ui/accessibility/unix/testcase/html/")
-#
-# checkboxes = dr.find_elements_by_xpath('//*[@id="radio2"]')
-# for checkbox in checkboxes:
-#     if checkbox.isSelected():
-#         checkbox.click()
+dropdown.find_element(By.XPATH, '/option[3]')
+dropdown.click()
+time.sleep(2)
 
-#Task4 Action
+'''Task2 Выбор из выпадающего меню (Select)'''
+dr = webdriver.Chrome()
+dr.get("https://www-archive.mozilla.org/projects/ui/accessibility/unix/testcase/html/")
+
+select = Select(dr.find_element(By.ID, 'components'))
+time.sleep(2)
+select.select_by_value('Build config')
+time.sleep(2)
+
+'''Task3 Checkbox ?????? (не разобралась до конца)'''
+dr = webdriver.Chrome()
+dr.get("https://www-archive.mozilla.org/projects/ui/accessibility/unix/testcase/html/")
+
+checkboxes = dr.find_elements_by_xpath('//*[@id="radio2"]')
+for checkbox in checkboxes:
+    if checkbox.isSelected():
+        checkbox.click()
+
+'''Task4 Action'''
 dr = webdriver.Chrome()
 actions = webdriver.ActionChains(dr)
 dr.get("https://novaposhta.ua/")
